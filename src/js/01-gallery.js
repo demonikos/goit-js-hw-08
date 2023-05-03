e243a0de319679f39bed4cb0c47b5442424ca2ea
demonikos <119507967+demonikos@users.noreply.github.com>
2023-05-04 00:18:55 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
 console.log(galleryItems);
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+const gallery = document.querySelector('ul');
 
-
-const gallery = document.querySelector(".gallery");
+// console.log(gallery.style)
+gallery.style.listStyle = 'none';
 
 const addItems = galleryItems
   .map(
-    (item) =>
+    item =>
       `<li class="gallery__item"> <a class="gallery__link" href="${item.original}"> <img class="gallery__image" src="${item.preview}" alt="${item.description}" /> </a></li>`
   )
-  .join("");
+  .join('');
 
-gallery.insertAdjacentHTML("afterbegin", addItems);
+gallery.insertAdjacentHTML('afterbegin', addItems);
 
 // console.log(addItems);
 
-let lightbox = new SimpleLightbox(".gallery li a", {
-  captionsData: "alt",
-  captionPosition: "bottom",
+let lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
   captionDelay: 250,
   scrollZoom: false,
 });
